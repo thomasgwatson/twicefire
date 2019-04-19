bb1df0faf6059f4fd2e45278a217ecf97091cb36
Tom Watson <thomasgabriel.watson@gmail.com>
2019-04-19 06:58:41 -0700

    Improving og spacing

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -225,11 +225,11 @@ const PageTemplate: React.FunctionComponent<PageTemplateProps> = props => {
         <html lang={config.lang} />
         <title>{post.frontmatter.title}</title>
 
-        <meta name="description" content={post.frontmatter.excerpt} />
-        <meta property="og:site_name" content={config.title} />
+        <meta name="description" content={` ${post.frontmatter.excerpt}`} />
+        <meta property="og:site_name" content={` ${config.title}`} />
         <meta property="og:type" content="article" />
-        <meta property="og:title" content={post.frontmatter.title} />
-        <meta property="og:description" content={post.frontmatter.excerpt} />
+        <meta property="og:title" content={` ${post.frontmatter.title}: `} />
+        <meta property="og:description" content={` ${post.frontmatter.title} `} />
         <meta property="og:url" content={config.siteUrl + props.pathContext.slug} />
         {post.frontmatter.image && (
           <meta property="og:image" content={post.frontmatter.image.childImageSharp.fluid.src} />
@@ -250,9 +250,9 @@ const PageTemplate: React.FunctionComponent<PageTemplateProps> = props => {
         {post.frontmatter.image && (
           <meta name="twitter:image" content={post.frontmatter.image.childImageSharp.fluid.src} />
         )}
-        <meta name="twitter:label1" content="Written by" />
+        <meta name="twitter:label1" content="Written by " />
         <meta name="twitter:data1" content={post.frontmatter.author.id} />
-        <meta name="twitter:label2" content="Filed under" />
+        <meta name="twitter:label2" content="Filed under " />
         {post.frontmatter.tags && <meta name="twitter:data2" content={post.frontmatter.tags[0]} />}
         <meta name="twitter:site" content={`@${config.twitter.split('https://twitter.com/')[0]}`} />
         <meta
